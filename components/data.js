75e61eab58271eeb20ecaafc3f6b2d07714d20cb
tmerlier <tmerlier@student.42.fr>
2017-12-20 12:05:53 +0100

    Merge PCI Vecteur and Image.

--- components/data.js
+++ components/data.js
@@ -1,5 +1,4 @@
 import FlaskIcon from 'react-icons/lib/fa/flask'
-import FaFileImageO from 'react-icons/lib/fa/file-image-o'
 import MapIcon from 'react-icons/lib/fa/map-o'
 import DownloadIcon from 'react-icons/lib/fa/download'
 import ThIcon from 'react-icons/lib/fa/th'
@@ -9,16 +8,10 @@ import HeadLinkTitle from './head-link-title'
 
 const titles = [
   {
-    title: 'PCI Vecteur',
-    href: '/data/pci-vecteur',
+    title: 'Plan Cadastral Informatisé (PCI)',
+    href: '/data/pci',
     subtitle: 'Le plan cadastral est le découpage du territoire français en unités de surfaces permettant le calcul de certains impôts (notamment la taxe foncière, la taxe d’habitation et la cotisation foncière des entreprises).',
     icon: <ThIcon />
-  },
-  {
-    title: 'PCI Image',
-    href: '/data/pci-image',
-    subtitle: '[insert description here]',
-    icon: <FaFileImageO />
   },
   {
     title: 'Cadastre par Etalab',
