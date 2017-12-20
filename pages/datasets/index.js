be83c97b47c62276936ad76bebbcd494b66ada96
tmerlier <tmerlier@student.42.fr>
2017-12-20 19:41:56 +0100

    Add title and description to /datasets

--- pages/datasets/index.js
+++ pages/datasets/index.js
@@ -5,7 +5,7 @@ import Head from '../../components/head'
 import Datasets from '../../components/datasets'
 
 export default () => (
-  <Page>
+  <Page title='Données brutes' description='cadaste.data.gouv.fr met en place des outils pour une prise en main rapide des données cadastre ouvertes.'>
     <Head title='Données brutes' icon={<DownloadIcon />}>
       <div><p>cadaste.data.gouv.fr met en place des outils pour une prise en main rapide des données cadastre ouvertes.</p></div>
     </Head>
