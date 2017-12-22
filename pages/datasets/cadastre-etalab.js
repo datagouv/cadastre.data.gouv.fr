1fb1da2c8936ad26e91c0f84f1b3e60a120e28ec
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2017-12-22 13:26:36 +0100

    Fix meta

--- pages/datasets/cadastre-etalab.js
+++ pages/datasets/cadastre-etalab.js
@@ -4,7 +4,7 @@ import Page from '../../layouts/main'
 import Head from '../../components/head'
 import CadastreEtalab from '../../components/cadastre-etalab'
 
-const title = 'Cadastre par Etalab'
+const title = 'Cadastre Etalab'
 const description = 'Données consolidées pour la France entière, produites par Etalab à partir des données de référence.'
 
 export default () => (
