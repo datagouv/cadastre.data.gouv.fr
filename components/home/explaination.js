706f6955a7e09ec0acbd46f1c8ef670e7f8a30d2
Jérôme Desboeufs <jerome@livingdata.co>
2017-12-22 21:21:51 +0100

    Fix typo

--- components/home/explaination.js
+++ components/home/explaination.js
@@ -2,7 +2,7 @@ import Section from '../section'
 
 const subtitle = `Le cadastre est un ensemble de plans et fichiers administratifs qui recensent
 toutes les propriétés immobilières situées dans chaque commune française, et qui en consigne
-leur valeur afin de servir de base de calcul à certains impôts. (Wikipedia)`
+la valeur afin de servir de base de calcul à certains impôts. (Wikipedia)`
 
 export default () => (
   <Section title='Qu’est-ce que le cadastre ?' subtitle={subtitle} />
