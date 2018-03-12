e695ed75140c2e80317c7ebed73831b0064716a6
Bertrand Marron <bertrand.marron@gmail.com>
2018-03-12 17:20:05 +0100

    Update to next5
    Fix issue with trailing slashes.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@ const {join} = require('path')
 const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer')
 
 module.exports = {
-  webpack(config, {dev}) {
-    if (!dev) {
+  webpack(config, {dev, isServer}) {
+    if (!dev && !isServer) {
       config.plugins.push(new BundleAnalyzerPlugin({
         analyzerMode: 'static',
         openAnalyzer: false,
