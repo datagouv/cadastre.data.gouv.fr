551ed9665fdb4df41c311eeae6e6e2991f92584d
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2019-04-23 18:28:11 +0200

    Enable DVF download link

--- pages/dvf.js
+++ pages/dvf.js
@@ -21,8 +21,8 @@ export default () => (
         <p>
           La base de données « Demandes de valeurs foncières », ou DVF, recense l’ensemble des ventes de biens fonciers réalisées au cours des cinq dernières années, en métropole et dans les départements et territoires d’outre-mer&nbsp;— sauf à Mayotte et en Alsace-Moselle. Les biens concernés peuvent être bâtis (appartement et maison) ou non bâtis (parcelles et exploitations). Les données sont produites par Bercy, c’est-à-dire par la direction générale des finances publiques. Elles proviennent des actes enregistrés chez les notaires et des informations contenues dans le cadastre.
         </p>
-        <ButtonLink className='button-outline large primary' href='#download'>
-          Télécharger les données
+        <ButtonLink className='button-outline large primary' href='https://cadastre.data.gouv.fr/data/hackathon-dgfip-dvf/'>
+          Télécharger les données DVF
         </ButtonLink>
       </div>
     </Section>
