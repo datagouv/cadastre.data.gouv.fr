4b01bf0a90632aeee4e7371706ec3ac09c0fc415
tmerlier <tmerlier@student.42.fr>
2019-04-26 10:48:50 +0200

    Use Link for ButtonLink

--- components/button-link.js
+++ components/button-link.js
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types'
+import Link from 'next/link'
 
-const ButtonLink = ({children, ...props}) => (
-  <a className='button' {...props}>
-    {children}
-  </a>
-)
+const ButtonLink = ({children, ...props}) => {
+  const {href, as} = props
+  return (
+    <Link href={href} as={as}>
+      <a className='button' {...props}>
+        {children}
+      </a>
+    </Link>
+  )
+}
 
 ButtonLink.propTypes = {
+  href: PropTypes.string.isRequired,
+  as: PropTypes.string,
   children: PropTypes.node
 }
 
 ButtonLink.defaultProps = {
+  as: null,
   children: null
 }
 
