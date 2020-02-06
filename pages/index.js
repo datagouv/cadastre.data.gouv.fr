3614374bace06727745afb66e2680eba4500103c
tmerlier <tmerlier@student.42.fr>
2020-02-06 18:16:21 +0100

    Add /map to nav and link in home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 
+import {Map} from 'react-feather'
+
 import Page from '../layouts/main'
 import Hero from '../components/hero'
 
@@ -7,6 +9,7 @@ import Usages from '../components/home/usages'
 import Subscribe from '../components/home/subscribe'
 import OtherSites from '../components/home/other-sites'
 import Section from '../components/section'
+import ButtonLink from '../components/button-link'
 
 const title = 'Données cadastrales ouvertes'
 const tagline = 'Consulter, télécharger et intégrer facilement les données cadastrales'
@@ -29,6 +32,26 @@ export default () => (
           Le cadastre est un ensemble de plans et fichiers administratifs qui recensent toutes les propriétés immobilières situées dans chaque commune française, et qui en consigne la valeur afin de servir de base de calcul à certains impôts. (Wikipedia)
           </p>
         </div>
+      </div>
+
+      <div className='action-button'>
+        <ButtonLink style={{
+          display: 'inline-flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          textAlign: 'center'
+        }} href='/map' size='large'
+        >
+          Consulter la carte du cadastre <span style={{margin: '0 0.5em'}}><Map /></span>
+        </ButtonLink>
+        <style jsx>{`
+          .action-button {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            margin-top: 2em;
+          }
+          `}</style>
       </div>
     </Section>
 
