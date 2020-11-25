4b36c714274b68c997f7b5429a861fcea7269ba0
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-25 22:05:16 +0100

    Remove exportPathMap

--- next.config.js
+++ next.config.js
@@ -2,16 +2,4 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true'
 })
 
-module.exports = withBundleAnalyzer({
-  exportPathMap() {
-    return {
-      '/': {page: '/'},
-      '/datasets': {page: '/datasets'},
-      '/mentions-legales': {page: '/mentions-legales'},
-      '/faq': {page: '/faq'},
-      '/datasets/plan-cadastral-informatise': {page: '/datasets/plan-cadastral-informatise'},
-      '/datasets/cadastre-strasbourg': {page: '/datasets/cadastre-strasbourg'},
-      '/datasets/cadastre-etalab': {page: '/datasets/cadastre-etalab'}
-    }
-  }
-})
+module.exports = withBundleAnalyzer({})
