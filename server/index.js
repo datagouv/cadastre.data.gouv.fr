43ca67b40625ce129fbb142c6af8871c1ce7e27a
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-11-25 22:05:44 +0100

    Fix custom server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,13 @@
+const {join} = require('path')
+const url = require('url')
 const express = require('express')
 const next = require('next')
 const compression = require('compression')
 
-const createRoutes = require('./routes')
-
 const port = parseInt(process.env.PORT, 10) || 3000
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   const server = express()
@@ -15,7 +16,15 @@ app.prepare().then(() => {
     server.use(compression())
   }
 
-  server.use('/', createRoutes(app))
+  server.get('/robots.txt', (req, res) => {
+    res.sendFile(join(__dirname, '..', 'public', 'static', 'robots.txt'))
+  })
+
+  server.get('*', (req, res) => {
+    /* eslint node/no-deprecated-api: off */
+    const parsedUrl = url.parse(req.url, true)
+    handle(req, res, parsedUrl)
+  })
 
   server.listen(port, err => {
     if (err) {
