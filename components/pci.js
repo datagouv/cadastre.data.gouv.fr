b123d4c4af12d306862b6bbed21e458a48f31656
Thomas Gratier <thomas_gratier@yahoo.fr>
2021-12-17 22:49:10 +0100

    MAJ Octobre 2021 pci part

--- components/pci.js
+++ components/pci.js
@@ -24,6 +24,17 @@ const products = [
 const millesimes = [
   {
     latest: true,
+    date: '1er octobre 2021',
+    path: '2021-10-01',
+    formats: [
+      {name: 'edigeo', granularities: ['feuilles', 'departements']},
+      {name: 'edigeo-cc', granularities: ['feuilles', 'departements']},
+      {name: 'dxf', granularities: ['feuilles', 'departements']},
+      {name: 'dxf-cc', granularities: ['feuilles', 'departements']},
+      {name: 'tiff', granularities: ['feuilles', 'departements']}
+    ]
+  },
+  {
     date: '1er juillet 2021',
     path: '2021-07-01',
     formats: [
