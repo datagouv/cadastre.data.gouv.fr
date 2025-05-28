dc8329821fbbb1bfbcc05ff6e9766f0e4a8ae175
Thomas Gratier <thomas_gratier@yahoo.fr>
2025-05-28 11:09:43 +0200

    Generate JSON from deliveries config

---
+++ build/index.js
@@ -0,0 +1,11 @@
+import fs from 'fs'
+// eslint-disable-next-line import/extensions
+import {millesimes as millesimesPci} from '../lib/pci.js'
+// eslint-disable-next-line import/extensions
+import {millesimes as millesimesCadastreEtalab} from '../lib/cadastre-etalab.js'
+
+const dataPci = JSON.stringify(millesimesPci, null, ' ')
+const dataCadastreEtalab = JSON.stringify(millesimesCadastreEtalab, null, ' ')
+
+fs.writeFileSync('./public/static/pci.json', dataPci)
+fs.writeFileSync('./public/static/cadastre-etalab.json', dataCadastreEtalab)
